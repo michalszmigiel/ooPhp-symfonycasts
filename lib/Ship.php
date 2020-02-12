ecae889077a4a8595039467dc763452b27706d8b
michalszmigiel <michal.r.szmigiel@gmail.com>
2020-02-12 14:18:18 +0100

    Ship class added, index refactor

---
+++ lib/Ship.php
@@ -0,0 +1,121 @@
+<?php
+
+
+class Ship
+{
+    private string $name;
+    private int $weaponPower = 0;
+    private int $jediFactor = 0;
+    private int $strength = 0;
+    private bool $underRepair;
+
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+        $this->underRepair = mt_rand(1, 100) < 30;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isFunctional(): bool
+    {
+        return !$this->underRepair;
+    }
+
+    /**
+     * @return int
+     */
+    public function getWeaponPower(): int
+    {
+        return $this->weaponPower;
+    }
+
+    /**
+     * @param int $weaponPower
+     */
+    public function setWeaponPower(int $weaponPower): void
+    {
+        $this->weaponPower = $weaponPower;
+    }
+
+    /**
+     * @return int
+     */
+    public function getJediFactor(): int
+    {
+        return $this->jediFactor;
+    }
+
+    /**
+     * @param int $jediFactor
+     */
+    public function setJediFactor(int $jediFactor): void
+    {
+        $this->jediFactor = $jediFactor;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStrength(): int
+    {
+        return $this->strength;
+    }
+
+    /**
+     * @param int $strength
+     */
+    public function setStrength(int $strength): void
+    {
+        $this->strength = $strength;
+    }
+
+    public function getNameAndSpecs($useShortFormat = false)
+    {
+        if($useShortFormat)
+        {
+            return sprintf(
+                "%s: %s/%s/%s",
+                $this->name,
+                $this->weaponPower,
+                $this->jediFactor,
+                $this->strength
+            );
+        }
+        else
+        {
+            return sprintf(
+                "%s: w:%s, j:%s, s:%s",
+                $this->name,
+                $this->weaponPower,
+                $this->jediFactor,
+                $this->strength
+            );
+        }
+
+    }
+
+    public function doesGivenShipHaveMoreStrength($givenShip)
+    {
+        return $givenShip->strength > $this->strength;
+    }
+
+}
---
+++ play.php
@@ -0,0 +1,27 @@
+<?php
+require __DIR__.'/lib/Ship.php';
+
+$myShip = new Ship();
+$myShip->name = 'Jedi Starship';
+$myShip->weaponPower = 10;
+
+
+$otherShip = new Ship();
+$otherShip->name = 'Imperial Shuttle';
+$otherShip->weaponPower = 5;
+$otherShip->strength = 50;
+
+echo $myShip->getNameAndSpecs(false);
+echo "<hr/>";
+echo $otherShip->getNameAndSpecs(false);
+echo "<hr/>";
+
+if($myShip->doesGivenShipHaveMoreStrength($otherShip))
+{
+    echo "$otherShip->name has more strength";
+}
+else
+{
+    echo "$myShip->name has more strength";
+}
+
