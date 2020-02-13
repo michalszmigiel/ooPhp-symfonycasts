fc4b1c8323d4e74ee2d3e4dfbb8af25d00a7f85e
michalszmigiel <michal.r.szmigiel@gmail.com>
2020-02-13 11:59:04 +0100

    call and override parent function

--- lib/Model/RebelShip.php
+++ lib/Model/RebelShip.php
@@ -32,27 +32,10 @@ class RebelShip extends Ship
 
     public function getNameAndSpecs($useShortFormat = false)
     {
-        if($useShortFormat)
-        {
-            return sprintf(
-                "%s: %s/%s/%s (Rebel)",
-                $this->getName(),
-                $this->getWeaponPower(),
-                $this->getJediFactor(),
-                $this->getStrength()
-            );
-        }
-        else
-        {
-            return sprintf(
-                "%s: w:%s, j:%s, s:%s (Rebel)",
-                $this->getName(),
-                $this->getWeaponPower(),
-                $this->getJediFactor(),
-                $this->getStrength()
-            );
-        }
+        $nameAndSpecsToReturn = parent::getNameAndSpecs($useShortFormat);
+        $nameAndSpecsToReturn .= ' (Rebel)';
 
+        return $nameAndSpecsToReturn;
     }
 
 }
