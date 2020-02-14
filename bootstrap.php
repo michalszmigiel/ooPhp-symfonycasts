1777e7a848afef9eaae6a7294c5453f0f34209d4
michalszmigiel <michal.r.szmigiel@gmail.com>
2020-02-14 13:38:52 +0100

    added composer with autoloader

--- bootstrap.php
+++ bootstrap.php
@@ -1,14 +1,6 @@
 <?php
 
-spl_autoload_register(function ($className)
-{
-    $path = __DIR__.'/lib/'.str_replace('\\', '/', $className).'.php';
-
-    if(file_exists($path))
-    {
-        require $path;
-    }
-});
+require __DIR__.'/vendor/autoload.php';
 
 $configuration = [
     'db_dsn' => 'mysql:host=localhost;dbname=oo_battle',
